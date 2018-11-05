cd04f8fe197f664f0397018ee0a3c1ec280bb28e
Mr.Aaron <807446126@qq.com>
2018-11-05 14:59:38 +0800

    firstCommit

---
+++ Example/Pods/Target Support Files/GWHomeKit/GWHomeKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GWHomeKit : NSObject
+@end
+@implementation PodsDummy_GWHomeKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-GWHomeKit_Tests/Pods-GWHomeKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GWHomeKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GWHomeKit_Tests
+@end
